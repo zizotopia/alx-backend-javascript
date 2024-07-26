2ca3a35a815934de1cd87129b2e0e5e560233c49
zizotopia <zizoknightboy07@yahoo.com>
2024-07-26 06:11:28 -0700

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,50 +1,52 @@
-/* eslint no-underscore-dangle: ["error", {"allow": ["_name", "_length", "_students"] }] */
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+   * @param {String} name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(Name) {
-    if (typeof Name === 'string') {
-      this._name = Name;
-    } else {
-      throw new TypeError('Name must be a string');
+  /**
+   * @param {Number} length
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    this._name = Name;
+    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(Length) {
-    if (typeof Length === 'number') {
-      this._length = Length;
+  /**
+   * @param {Array} students
+   */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
     } else {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('Students must be an Array');
     }
   }
 
   get students() {
     return this._students;
   }
-
-  set students(Students) {
-    if (typeof Students === 'object') {
-      for (const student in Students) {
-        if (typeof student !== 'string') {
-          throw new TypeError('Student must be an array');
-        }
-      }
-    } else {
-      throw new TypeError('Student must be an array of strings');
-    }
-    this._students = Students;
-  }
 }
+
+export default HolbertonCourse;
